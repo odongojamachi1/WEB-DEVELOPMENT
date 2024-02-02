3df94fef48b7bb5b813f64fdb3e902f594b6dbde
odongojamachi1 <odongojamachi1@gmail.com>
2024-02-02 13:12:03 +0300

    hello world

---
+++ dd/head.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-sm bg-dark">
+            <div class="container-fluid">
+                  <ul class="navbar-nav">
+                        <ul class="nav">
+
+                             <li class="nave-item">
+                                 <a class="nav-link" href="HOME.html">HOME</a>
+                                      </li>
+
+                                     <li class="nave-item">
+                                <a class="nav-link" href="forms.html">FORMS</FORM></a>
+                            </li>
+
+                        <li class="nave-item">
+                    <a class="nav-link" href="SERVICE.html">SERVICE</a>
+                </li>
+
+            <li class="nave-item">
+        <a class="nav-link" href="ABOUT.html">ABOUT</a>
+    </li>
+
+    <li class="nave-item">
+        <a class="nav-link" href="ODER.html">ODER</a>
+    </li>  
+
+</ul>
+</nav>
+</ul>
+</div>
---
+++ proccess.php
@@ -0,0 +1,10 @@
+<?php
+
+if ($_POST["Email"] && $_POST["Password"]) {
+    echo "yor request was successfuly submited"
+}
+
+else
+    echo "your input was invalid"
+
+?>
